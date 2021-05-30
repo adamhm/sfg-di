1012f854157e7120132656cabc24f423fbbfa44b
Adam Hamar <adam.hamar@gmail.com>
2021-05-30 20:05:10 +0200

    Test dependency injections

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
 import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,6 +20,26 @@ public class SfgDiApplication {
         );
         
         System.out.println(myController.sayHello());
+        System.out.println("============= Property");
+        PropertyInjectedController propertyInjectedController = ctx.getBean(
+            PropertyInjectedController.class,
+            "propertyInjectedController"
+        );
+        System.out.println(propertyInjectedController.getGreeting());
+        
+        System.out.println("============= Setter");
+        SetterInjectedController setterInjectedController = ctx.getBean(
+            SetterInjectedController.class,
+            "setterInjectedController"
+        );
+        System.out.println(setterInjectedController.getGreeting());
+        
+        System.out.println("============= Constructor");
+        ConstructorInjectedController constructorInjectedController = ctx.getBean(
+            ConstructorInjectedController.class,
+            "constructorInjectedController"
+        );
+        System.out.println(constructorInjectedController.getGreeting());
     }
     
 }
