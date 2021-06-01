42f0f38fd6f58c6d9fd56a5ba293b1799d5551ff
Adam Hamar <adam.hamar@gmail.com>
2021-06-01 08:32:18 +0200

    Test the i18n controller

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.PropertyInjectedController;
-import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,13 @@ public class SfgDiApplication {
     
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+        
+        System.out.println("============= Primary Bean");
+        I18nController i18nController = ctx.getBean(
+            I18nController.class,
+            "i18nController"
+        );
+        System.out.println(i18nController.getGreeting());
         
         System.out.println("============= Primary Bean");
         MyController myController = ctx.getBean(
