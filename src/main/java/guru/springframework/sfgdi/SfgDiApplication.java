822c749b4ffa447da6484f891bf9358c3e5eeb8f
Adam Hamar <adam.hamar@gmail.com>
2021-06-04 09:14:23 +0200

    Test bean scopes

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.services.PrototypeBean;
+import guru.springframework.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,6 +14,7 @@ public class SfgDiApplication {
     
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+        
         
         System.out.println("============= The best pet is: ");
         PetController petController = ctx.getBean("petController", PetController.class);
@@ -51,6 +54,16 @@ public class SfgDiApplication {
             "constructorInjectedController"
         );
         System.out.println(constructorInjectedController.getGreeting());
+        
+        System.out.println("-------- Bean Scopes --------");
+        SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean1.getMyScope());
+        SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean2.getMyScope());
+        PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean1.getMyScope());
+        PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean2.getMyScope());
     }
     
 }
